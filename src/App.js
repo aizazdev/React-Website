b90be708c8781ea3a8891715fa81b71494b52f73
Aizaz Ahmad <65584802+aizazdev@users.noreply.github.com>
2021-03-01 23:31:55 -0800

    Remove Unecessary things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import React, { useEffect, useState, createContext } from 'react';
+import React from 'react';
 
 
 const App = ()=> {
-
-    const context = createContext('silver');
-
     return(
         <div>
             Hello World From Aizaz
