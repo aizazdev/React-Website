31c6853694279b51d5fdcce4ede3072df65a9db3
Aizaz Ahmad <65584802+aizazdev@users.noreply.github.com>
2021-03-01 23:26:26 -0800

    Changes app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { useEffect, useState, createContext } from 'react';
-import Cards from './Components/Cards/Cards';
-import Chart from './Components/Charts/Chart';
-import CountryPicker from './Components/CountryPicker/CountryPicker';
-import {fetchData} from './api/api';
 
 
 const App = ()=> {
@@ -10,17 +6,9 @@ const App = ()=> {
     const context = createContext('silver');
 
     return(
-        <>
-        <createContext.Provider value={context}>
-            <div>
-                <center>
-                    <Cards />
-                    <Chart />
-                    <CountryPicker />
-                </center>
-            </div>
-        </createContext.Provider>
-        </>
+        <div>
+            Hello World From Aizaz
+        </div>
     )
 }
 
